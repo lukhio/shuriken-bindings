54a692ddd0529865a5acfe78b3deb61582b4d1f0
Julien Gamba <julien@jgamba.eu>
2025-01-08 23:20:10 +0100

    Basic wrapper around `parse_dex` to test things out

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,19 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
 
-include!(concat!(env!("OUT_DIR"), "/shuriken_core.rs"));
+use std::ffi::CString;
+
+mod shuriken {
+    #![allow(dead_code)]
+    include!(concat!(env!("OUT_DIR"), "/shuriken_core.rs"));
+}
+
+/// Parse a Dex file
+/// TODO: change for pathbuf
+pub fn parse_dex(filepath: String) -> shuriken::hDexContext {
+    let c_str = CString::new(filepath).unwrap();
+    let c_world = c_str.as_ptr();
+    unsafe {
+        shuriken::parse_dex(c_world)
+    }
+}
