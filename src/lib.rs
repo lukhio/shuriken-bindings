03a070ee1fc3e36b652d15afc6ff5a13b022e415
Julien Gamba <julien@jgamba.eu>
2025-01-13 21:42:09 +0100

    Disassemble DEX and tests

--- src/lib.rs
+++ src/lib.rs
@@ -399,7 +399,7 @@ impl DvmInstruction {
             99 => DexInstType::DexNoneOp,
             _  => DexInstType::DexDalvikIncorrect,
         };
-            
+
         let disassembly = unsafe {
             CStr::from_ptr(ins.disassembly)
                 .to_str()
@@ -420,17 +420,111 @@ impl DvmInstruction {
 /// Type alias for Shuriken's `dvmhandler_data_t`
 ///
 /// Structure that keeps information about a handler
-pub struct DvmHandlerData(shuriken::dvmhandler_data_t);
+#[derive(Debug)]
+pub struct DvmHandlerData {
+    handler_type: String,
+    handler_start_addr: u64
+}
+
+impl DvmHandlerData {
+    fn from_ptr(ptr: shuriken::dvmhandler_data_t) -> Self {
+        let handler_type = unsafe {
+            CStr::from_ptr(ptr.handler_type)
+                .to_str()
+                .expect("Error: string is not valid UTF-8")
+                .to_string()
+        };
+
+        Self {
+            handler_type,
+            handler_start_addr: ptr.handler_start_addr
+        }
+    }
+}
 
 /// Type alias for Shuriken's `dvmexceptions_data_t`
 ///
 /// Structure with the information from the exceptions in the code
-pub struct DvmException(shuriken::dvmexceptions_data_t);
+#[derive(Debug)]
+pub struct DvmException {
+    try_value_start_addr: u64,
+    try_value_end_addr: u64,
+    n_of_handlers: usize,
+    handlers: Vec<DvmHandlerData>
+}
+
+impl DvmException {
+    fn from_ptr(ptr: shuriken::dvmexceptions_data_t) -> Self {
+        let handlers = unsafe {
+            from_raw_parts(ptr.handler, ptr.n_of_handlers)
+                .iter()
+                .map(|handler| DvmHandlerData::from_ptr(*handler))
+                .collect::<Vec<DvmHandlerData>>()
+        };
+
+        Self {
+            try_value_start_addr: ptr.try_value_start_addr,
+            try_value_end_addr: ptr.try_value_end_addr,
+            n_of_handlers: ptr.n_of_handlers,
+            handlers
+        }
+    }
+}
 
 /// Type alias for Shuriken's `dvmdisassembled_method_t`
 ///
 /// Structure that represents a disassembled method from the dalvik file
-pub struct DvmDisassembledMethod(shuriken::dvmdisassembled_method_t);
+#[derive(Debug)]
+pub struct DvmDisassembledMethod {
+    // TODO: replace with ref maybe?
+    method_id: DvmMethod,
+    n_of_registers: usize,
+    n_of_exceptions: usize,
+    exception_information: Vec<DvmException>,
+    n_of_instructions: usize,
+    instructions: Vec<DvmInstruction>,
+    method_string: String,
+}
+
+impl DvmDisassembledMethod {
+    fn from_dvmdisassembled_method_t(
+        dvm_disas: shuriken::dvmdisassembled_method_t,
+        dvm_method: DvmMethod
+    ) -> Self
+    {
+
+        let method_string = unsafe {
+            CStr::from_ptr(dvm_disas.method_string)
+                .to_str()
+                .expect("Error: string is not valid UTF-8")
+                .to_string()
+        };
+
+        let exception_information = unsafe {
+            from_raw_parts(dvm_disas.exception_information, dvm_disas.n_of_exceptions)
+                .iter()
+                .map(|exc| DvmException::from_ptr(*exc))
+                .collect::<Vec<DvmException>>()
+        };
+
+        let instructions = unsafe {
+            from_raw_parts(dvm_disas.instructions, dvm_disas.n_of_instructions)
+                .iter()
+                .map(|ins| DvmInstruction::from_ins(*ins))
+                .collect::<Vec<DvmInstruction>>()
+        };
+
+        Self {
+            method_id: dvm_method,
+            n_of_registers: dvm_disas.n_of_registers.into(),
+            n_of_exceptions: dvm_disas.n_of_exceptions,
+            exception_information,
+            n_of_instructions: dvm_disas.n_of_instructions,
+            instructions,
+            method_string
+        }
+    }
+}
 
 // --------------------------- Analysis Data ---------------------------
 
@@ -591,12 +685,26 @@ impl DexContext {
 
     /// Disassemble a DEX file and generate an internal DexDisassembler
     pub fn disassemble_dex(&self) {
-        todo!();
+        unsafe {
+            shuriken::disassemble_dex(self.0)
+        }
     }
 
     /// Get a method structure given a full dalvik name.
-    pub fn get_disassembled_method(&self, method_name: String) -> DvmDisassembledMethod {
-        todo!();
+    pub fn get_disassembled_method(&self, method_name: &str) -> Option<DvmDisassembledMethod> {
+        let c_str = CString::new(method_name)
+            .expect("CString::new failed");
+
+        let dvm_method = self.get_method_by_name(method_name)
+                             .expect("Cannot find function");
+        let dvm_disas = unsafe { shuriken::get_disassembled_method(self.0, c_str.as_ptr()) };
+
+        if ! dvm_disas.is_null() {
+            Some(unsafe { DvmDisassembledMethod::from_dvmdisassembled_method_t(*dvm_disas, dvm_method) })
+        } else {
+            eprintln!("No disassembled method. Did you run `DexContext::disassemble_dex()`?");
+            None
+        }
     }
 
     // --------------------------- Analysis API ---------------------------
@@ -974,6 +1082,152 @@ mod tests {
             assert_eq!(method.as_ref().unwrap().class_name, "LDexParserTest;");
             assert_eq!(method.as_ref().unwrap().prototype, "()V");
             assert_eq!(method.as_ref().unwrap().access_flags, vec![DvmAccessFlag::ACC_PRIVATE]);
+        }
+
+        #[test]
+        fn test_disassemble_dex() {
+            let paths = fs::read_dir(TEST_FILES_PATH).unwrap();
+
+            for path in paths {
+                let path = path.unwrap().path();
+
+                // Only testing DEX files
+                if path.extension().unwrap() == "apk" {
+                    continue;
+                }
+
+                let context = DexContext::parse_dex(&path);
+                context.disassemble_dex();
+            }
+        }
+
+        #[test]
+        fn test_get_disassembled_method() {
+            use std::collections::HashMap;
+
+            let methods = HashMap::from([
+                (
+                    String::from("LDexParserTest;-><init>()V"),
+                    vec![
+                        ".method constructor public LDexParserTest;-><init>()V",
+                        ".registers 2",
+                        "00000000 invoke-direct {v1}, Ljava/lang/Object;-><init>()V // method@5",
+                        "00000006 const/16 v0, 42",
+                        "0000000a iput v0, v1, DexParserTest->field1 int // field@0",
+                        "0000000e const-string v0, \"Hello, Dex Parser!\" // string@6",
+                        "00000012 iput-object v0, v1, DexParserTest->field2 java.lang.String // field@1",
+                        "00000016 return-void",
+                        ".end method"
+                    ]
+                ),
+                (
+                    String::from("LDexParserTest;->calculateSum(II)I"),
+                    vec![
+                        ".method private LDexParserTest;->calculateSum(II)I",
+                        ".registers 7",
+                        "00000000 add-int v0, v5, v6",
+                        "00000004 sget-object v1, java.lang.System->out java.io.PrintStream // field@2",
+                        "00000008 new-instance v2, Ljava/lang/StringBuilder; // type@5",
+                        "0000000c invoke-direct {v2}, Ljava/lang/StringBuilder;-><init>()V // method@6",
+                        "00000012 const-string v3, \"Sum of \" // string@18",
+                        "00000016 invoke-virtual {v2, v3}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "0000001c move-result-object v2",
+                        "0000001e invoke-virtual {v2, v5}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "00000024 move-result-object v5",
+                        "00000026 const-string v2, \" and \" // string@0",
+                        "0000002a invoke-virtual {v5, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000030 move-result-object v5",
+                        "00000032 invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "00000038 move-result-object v5",
+                        "0000003a const-string v6, \" is: \" // string@1",
+                        "0000003e invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000044 move-result-object v5",
+                        "00000046 invoke-virtual {v5, v0}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "0000004c move-result-object v5",
+                        "0000004e invoke-virtual {v5}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String; // method@9",
+                        "00000054 move-result-object v5",
+                        "00000056 invoke-virtual {v1, v5}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
+                        "0000005c return v0",
+                        ".end method"
+                    ]
+                ),
+                (
+                    String::from("LDexParserTest;->main([Ljava/lang/String;)V"),
+                    vec![
+                        ".method public static LDexParserTest;->main([Ljava/lang/String;)V",
+                        ".registers 3",
+                        "00000000 new-instance v2, LDexParserTest; // type@1",
+                        "00000004 invoke-direct {v2}, LDexParserTest;-><init>()V // method@0",
+                        "0000000a invoke-direct {v2}, LDexParserTest;->printMessage()V // method@3",
+                        "00000010 const/16 v0, 10",
+                        "00000014 const/16 v1, 20",
+                        "00000018 invoke-direct {v2, v0, v1}, LDexParserTest;->calculateSum(II)I // method@1",
+                        "0000001e return-void",
+                        ".end method"
+                    ]
+                ),
+                (
+                    String::from("LDexParserTest;->printMessage()V"),
+                    vec![
+                        ".method private LDexParserTest;->printMessage()V",
+                        ".registers 4",
+                        "00000000 sget-object v0, java.lang.System->out java.io.PrintStream // field@2",
+                        "00000004 new-instance v1, Ljava/lang/StringBuilder; // type@5",
+                        "00000008 invoke-direct {v1}, Ljava/lang/StringBuilder;-><init>()V // method@6",
+                        "0000000e const-string v2, \"Field 1: \" // string@4",
+                        "00000012 invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000018 move-result-object v1",
+                        "0000001a iget v2, v3, DexParserTest->field1 int // field@0",
+                        "0000001e invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "00000024 move-result-object v1",
+                        "00000026 invoke-virtual {v1}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String; // method@9",
+                        "0000002c move-result-object v1",
+                        "0000002e invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
+                        "00000034 sget-object v0, java.lang.System->out java.io.PrintStream // field@2",
+                        "00000038 new-instance v1, Ljava/lang/StringBuilder; // type@5",
+                        "0000003c invoke-direct {v1}, Ljava/lang/StringBuilder;-><init>()V // method@6",
+                        "00000042 const-string v2, \"Field 2: \" // string@5",
+                        "00000046 invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "0000004c move-result-object v1",
+                        "0000004e iget-object v2, v3, DexParserTest->field2 java.lang.String // field@1",
+                        "00000052 invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000058 move-result-object v1",
+                        "0000005a invoke-virtual {v1}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String; // method@9",
+                        "00000060 move-result-object v1",
+                        "00000062 invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
+                        "00000068 sget-object v0, java.lang.System->out java.io.PrintStream // field@2",
+                        "0000006c const-string v1, \"This is a test message printed from DexParserTest class.\" // string@19",
+                        "00000070 invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
+                        "00000076 return-void",
+                        ".end method"
+                    ]
+                )
+            ]);
+
+            let context = DexContext::parse_dex(&PathBuf::from("test_files/DexParserTest.dex"));
+
+            // Check that we get nothing if we have not run `DexContext::disassemble_dex()`
+            let dvm_method = context.get_disassembled_method(
+                "LDexParserTest;->printMessage()V"
+            );
+            assert!(dvm_method.is_none());
+
+            context.disassemble_dex();
+
+            for (method, code) in methods.iter() {
+                let dvm_method = context.get_disassembled_method(&method);
+                assert!(dvm_method.is_some());
+
+                let dvm_method = dvm_method.unwrap();
+                assert_eq!(dvm_method.method_string
+                                     .split("\n")
+                                     .zip(*&code)
+                                     .filter(|&(a, b)| a != *b)
+                                     .map(|x| println!("{x:?}"))
+                                     .count(),
+                    0
+                );
+            }
         }
     }
 }
