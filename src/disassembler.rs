91aec437cb45200beb42841c7d7aff3ebb2d569e
Julien Gamba <julien@jgamba.eu>
2025-02-06 12:27:06 +0100

    Moved disassembler structs to separate file

---
+++ src/disassembler.rs
@@ -0,0 +1,226 @@
+//! Disassembler data structs
+
+use std::ffi::CStr;
+use std::slice::from_raw_parts;
+
+use crate::shuriken;
+use crate::parser::DvmMethod;
+
+/// Type alias for Shuriken's `dexinsttype_e`
+///
+/// Instruction types from the Dalvik Virtual Machine
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum DexInstType {
+    DexInstruction00x,
+    DexInstruction10x,
+    DexInstruction12x,
+    DexInstruction11n,
+    DexInstruction11x,
+    DexInstruction10t,
+    DexInstruction20t,
+    DexInstruction20bc,
+    DexInstruction22x,
+    DexInstruction21t,
+    DexInstruction21s,
+    DexInstruction21h,
+    DexInstruction21c,
+    DexInstruction23x,
+    DexInstruction22b,
+    DexInstruction22t,
+    DexInstruction22s,
+    DexInstruction22c,
+    DexInstruction22cs,
+    DexInstruction30t,
+    DexInstruction32x,
+    DexInstruction31i,
+    DexInstruction31t,
+    DexInstruction31c,
+    DexInstruction35c,
+    DexInstruction3rc,
+    DexInstruction45cc,
+    DexInstruction4rcc,
+    DexInstruction51l,
+    DexPackedSwitch,
+    DexSparseSwitch,
+    DexFillArrayData,
+    DexDalvikIncorrect,
+    DexNoneOp = 99,
+}
+
+/// Type alias for Shuriken's `hdvminstruction_t`
+///
+/// Structure for an instruction in the dalvik virtual machine
+#[derive(Debug)]
+pub struct DvmInstruction {
+    instruction_type: DexInstType,
+    instruction_length: usize,
+    address: u64,
+    // TODO: replace with a enum of all opcodes maybe?
+    op: u32,
+    disassembly: String
+}
+
+impl DvmInstruction {
+    pub fn from_ins(ins: shuriken::hdvminstruction_t) -> Self {
+        let instruction_type = match ins.instruction_type {
+            0  => DexInstType::DexInstruction00x,
+            1  => DexInstType::DexInstruction10x,
+            2  => DexInstType::DexInstruction12x,
+            3  => DexInstType::DexInstruction11n,
+            4  => DexInstType::DexInstruction11x,
+            5  => DexInstType::DexInstruction10t,
+            6  => DexInstType::DexInstruction20t,
+            7  => DexInstType::DexInstruction20bc,
+            8  => DexInstType::DexInstruction22x,
+            9  => DexInstType::DexInstruction21t,
+            10 => DexInstType::DexInstruction21s,
+            11 => DexInstType::DexInstruction21h,
+            12 => DexInstType::DexInstruction21c,
+            13 => DexInstType::DexInstruction23x,
+            14 => DexInstType::DexInstruction22b,
+            15 => DexInstType::DexInstruction22t,
+            16 => DexInstType::DexInstruction22s,
+            17 => DexInstType::DexInstruction22c,
+            18 => DexInstType::DexInstruction22cs,
+            19 => DexInstType::DexInstruction30t,
+            20 => DexInstType::DexInstruction32x,
+            21 => DexInstType::DexInstruction31i,
+            22 => DexInstType::DexInstruction31t,
+            23 => DexInstType::DexInstruction31c,
+            24 => DexInstType::DexInstruction35c,
+            25 => DexInstType::DexInstruction3rc,
+            26 => DexInstType::DexInstruction45cc,
+            27 => DexInstType::DexInstruction4rcc,
+            28 => DexInstType::DexInstruction51l,
+            29 => DexInstType::DexPackedSwitch,
+            30 => DexInstType::DexSparseSwitch,
+            31 => DexInstType::DexFillArrayData,
+            99 => DexInstType::DexNoneOp,
+            _  => DexInstType::DexDalvikIncorrect,
+        };
+
+        let disassembly = unsafe {
+            CStr::from_ptr(ins.disassembly)
+                .to_str()
+                .expect("Error: string is not valid UTF-8")
+                .to_string()
+        };
+
+        DvmInstruction {
+            instruction_type,
+            instruction_length: ins.instruction_length as usize,
+            address: ins.address,
+            op: ins.op,
+            disassembly
+        }
+    }
+}
+
+/// Type alias for Shuriken's `dvmhandler_data_t`
+///
+/// Structure that keeps information about a handler
+#[derive(Debug)]
+pub struct DvmHandlerData {
+    handler_type: String,
+    handler_start_addr: u64
+}
+
+impl DvmHandlerData {
+    fn from_ptr(ptr: shuriken::dvmhandler_data_t) -> Self {
+        let handler_type = unsafe {
+            CStr::from_ptr(ptr.handler_type)
+                .to_str()
+                .expect("Error: string is not valid UTF-8")
+                .to_string()
+        };
+
+        Self {
+            handler_type,
+            handler_start_addr: ptr.handler_start_addr
+        }
+    }
+}
+
+/// Type alias for Shuriken's `dvmexceptions_data_t`
+///
+/// Structure with the information from the exceptions in the code
+#[derive(Debug)]
+pub struct DvmException {
+    try_value_start_addr: u64,
+    try_value_end_addr: u64,
+    n_of_handlers: usize,
+    handlers: Vec<DvmHandlerData>
+}
+
+impl DvmException {
+    fn from_ptr(ptr: shuriken::dvmexceptions_data_t) -> Self {
+        let handlers = unsafe {
+            from_raw_parts(ptr.handler, ptr.n_of_handlers)
+                .iter()
+                .map(|handler| DvmHandlerData::from_ptr(*handler))
+                .collect::<Vec<DvmHandlerData>>()
+        };
+
+        Self {
+            try_value_start_addr: ptr.try_value_start_addr,
+            try_value_end_addr: ptr.try_value_end_addr,
+            n_of_handlers: ptr.n_of_handlers,
+            handlers
+        }
+    }
+}
+
+/// Type alias for Shuriken's `dvmdisassembled_method_t`
+///
+/// Structure that represents a disassembled method from the dalvik file
+#[derive(Debug)]
+pub struct DvmDisassembledMethod {
+    // TODO: replace with ref maybe?
+    method_id: DvmMethod,
+    n_of_registers: usize,
+    n_of_exceptions: usize,
+    exception_information: Vec<DvmException>,
+    n_of_instructions: usize,
+    instructions: Vec<DvmInstruction>,
+    method_string: String,
+}
+
+impl DvmDisassembledMethod {
+    pub fn from_dvmdisassembled_method_t(
+        dvm_disas: shuriken::dvmdisassembled_method_t,
+        dvm_method: DvmMethod
+    ) -> Self
+    {
+
+        let method_string = unsafe {
+            CStr::from_ptr(dvm_disas.method_string)
+                .to_str()
+                .expect("Error: string is not valid UTF-8")
+                .to_string()
+        };
+
+        let exception_information = unsafe {
+            from_raw_parts(dvm_disas.exception_information, dvm_disas.n_of_exceptions)
+                .iter()
+                .map(|exc| DvmException::from_ptr(*exc))
+                .collect::<Vec<DvmException>>()
+        };
+
+        let instructions = unsafe {
+            from_raw_parts(dvm_disas.instructions, dvm_disas.n_of_instructions)
+                .iter()
+                .map(|ins| DvmInstruction::from_ins(*ins))
+                .collect::<Vec<DvmInstruction>>()
+        };
+
+        Self {
+            method_id: dvm_method,
+            n_of_registers: dvm_disas.n_of_registers.into(),
+            n_of_exceptions: dvm_disas.n_of_exceptions,
+            exception_information,
+            n_of_instructions: dvm_disas.n_of_instructions,
+            instructions,
+            method_string
+        }
+    }
+}
