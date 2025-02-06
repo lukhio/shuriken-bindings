5ee5fa3327dabc3419d32ba43a8e9992c69017e2
Julien Gamba <julien@jgamba.eu>
2025-02-06 14:06:02 +0100

    Basic blocks test

--- src/lib.rs
+++ src/lib.rs
@@ -755,70 +755,9 @@ mod tests {
 
             let methods = HashMap::from([
                 (
-                    String::from("LDexParserTest;-><init>()V"),
-                    vec![
-                        ".method constructor public LDexParserTest;-><init>()V",
-                        ".registers 2",
-                        "00000000 invoke-direct {v1}, Ljava/lang/Object;-><init>()V // method@5",
-                        "00000006 const/16 v0, 42",
-                        "0000000a iput v0, v1, DexParserTest->field1 int // field@0",
-                        "0000000e const-string v0, \"Hello, Dex Parser!\" // string@6",
-                        "00000012 iput-object v0, v1, DexParserTest->field2 java.lang.String // field@1",
-                        "00000016 return-void",
-                        ".end method"
-                    ]
-                ),
-                (
-                    String::from("LDexParserTest;->calculateSum(II)I"),
-                    vec![
-                        ".method private LDexParserTest;->calculateSum(II)I",
-                        ".registers 7",
-                        "00000000 add-int v0, v5, v6",
-                        "00000004 sget-object v1, java.lang.System->out java.io.PrintStream // field@2",
-                        "00000008 new-instance v2, Ljava/lang/StringBuilder; // type@5",
-                        "0000000c invoke-direct {v2}, Ljava/lang/StringBuilder;-><init>()V // method@6",
-                        "00000012 const-string v3, \"Sum of \" // string@18",
-                        "00000016 invoke-virtual {v2, v3}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
-                        "0000001c move-result-object v2",
-                        "0000001e invoke-virtual {v2, v5}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
-                        "00000024 move-result-object v5",
-                        "00000026 const-string v2, \" and \" // string@0",
-                        "0000002a invoke-virtual {v5, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
-                        "00000030 move-result-object v5",
-                        "00000032 invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
-                        "00000038 move-result-object v5",
-                        "0000003a const-string v6, \" is: \" // string@1",
-                        "0000003e invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
-                        "00000044 move-result-object v5",
-                        "00000046 invoke-virtual {v5, v0}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
-                        "0000004c move-result-object v5",
-                        "0000004e invoke-virtual {v5}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String; // method@9",
-                        "00000054 move-result-object v5",
-                        "00000056 invoke-virtual {v1, v5}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
-                        "0000005c return v0",
-                        ".end method"
-                    ]
-                ),
-                (
-                    String::from("LDexParserTest;->main([Ljava/lang/String;)V"),
-                    vec![
-                        ".method public static LDexParserTest;->main([Ljava/lang/String;)V",
-                        ".registers 3",
-                        "00000000 new-instance v2, LDexParserTest; // type@1",
-                        "00000004 invoke-direct {v2}, LDexParserTest;-><init>()V // method@0",
-                        "0000000a invoke-direct {v2}, LDexParserTest;->printMessage()V // method@3",
-                        "00000010 const/16 v0, 10",
-                        "00000014 const/16 v1, 20",
-                        "00000018 invoke-direct {v2, v0, v1}, LDexParserTest;->calculateSum(II)I // method@1",
-                        "0000001e return-void",
-                        ".end method"
-                    ]
-                ),
-                (
                     String::from("LDexParserTest;->printMessage()V"),
                     vec![
-                        ".method private LDexParserTest;->printMessage()V",
-                        ".registers 4",
+                        "BB.0-120",
                         "00000000 sget-object v0, java.lang.System->out java.io.PrintStream // field@2",
                         "00000004 new-instance v1, Ljava/lang/StringBuilder; // type@5",
                         "00000008 invoke-direct {v1}, Ljava/lang/StringBuilder;-><init>()V // method@6",
@@ -847,10 +786,67 @@ mod tests {
                         "0000006c const-string v1, \"This is a test message printed from DexParserTest class.\" // string@19",
                         "00000070 invoke-virtual {v0, v1}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
                         "00000076 return-void",
-                        ".end method"
                     ]
-                )
+                ),
+
+                (
+                    String::from("LDexParserTest;->main([Ljava/lang/String;)V"),
+                    vec![
+                        "BB.0-32",
+                        "00000000 new-instance v2, LDexParserTest; // type@1",
+                        "00000004 invoke-direct {v2}, LDexParserTest;-><init>()V // method@0",
+                        "0000000a invoke-direct {v2}, LDexParserTest;->printMessage()V // method@3",
+                        "00000010 const/16 v0, 10",
+                        "00000014 const/16 v1, 20",
+                        "00000018 invoke-direct {v2, v0, v1}, LDexParserTest;->calculateSum(II)I // method@1",
+                        "0000001e return-void",
+                    ]
+                ),
+
+                (
+                    String::from("LDexParserTest;->calculateSum(II)I"),
+                    vec![
+                        "BB.0-94",
+                        "00000000 add-int v0, v5, v6",
+                        "00000004 sget-object v1, java.lang.System->out java.io.PrintStream // field@2",
+                        "00000008 new-instance v2, Ljava/lang/StringBuilder; // type@5",
+                        "0000000c invoke-direct {v2}, Ljava/lang/StringBuilder;-><init>()V // method@6",
+                        "00000012 const-string v3, \"Sum of \" // string@18",
+                        "00000016 invoke-virtual {v2, v3}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "0000001c move-result-object v2",
+                        "0000001e invoke-virtual {v2, v5}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "00000024 move-result-object v5",
+                        "00000026 const-string v2, \" and \" // string@0",
+                        "0000002a invoke-virtual {v5, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000030 move-result-object v5",
+                        "00000032 invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "00000038 move-result-object v5",
+                        "0000003a const-string v6, \" is: \" // string@1",
+                        "0000003e invoke-virtual {v5, v6}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder; // method@8",
+                        "00000044 move-result-object v5",
+                        "00000046 invoke-virtual {v5, v0}, Ljava/lang/StringBuilder;->append(I)Ljava/lang/StringBuilder; // method@7",
+                        "0000004c move-result-object v5",
+                        "0000004e invoke-virtual {v5}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String; // method@9",
+                        "00000054 move-result-object v5",
+                        "00000056 invoke-virtual {v1, v5}, Ljava/io/PrintStream;->println(Ljava/lang/String;)V // method@4",
+                        "0000005c return v0",
+                    ]
+                ),
+
+                (
+                    String::from("LDexParserTest;-><init>()V"),
+                    vec![
+                        "BB.0-24",
+                        "00000000 invoke-direct {v1}, Ljava/lang/Object;-><init>()V // method@5",
+                        "00000006 const/16 v0, 42",
+                        "0000000a iput v0, v1, DexParserTest->field1 int // field@0",
+                        "0000000e const-string v0, \"Hello, Dex Parser!\" // string@6",
+                        "00000012 iput-object v0, v1, DexParserTest->field2 java.lang.String // field@1",
+                        "00000016 return-void",
+                    ]
+                ),
             ]);
+
 
             let mut context = DexContext::parse_dex(&PathBuf::from("test_files/DexParserTest.dex"));
             context.disassemble_dex();
@@ -858,29 +854,36 @@ mod tests {
             context.analyze_classes();
 
             for idx in 0..context.get_number_of_classes() {
-                let class = context.get_class_by_id(idx as u16).unwrap();
-                let class_name = class.class_name();
-                println!("class name: {class_name:#?}");
-                let class_analysis = context.get_analyzed_class(class_name);
-                println!("class_analysis: {class_analysis:#?}");
-                break;
+                let class = context.get_class_by_id(idx as u16);
+                assert!(class.is_some());
+                let class = class.unwrap();
 
-                /*
-                for jdx in 0..class_analysis.met
-                    auto method_analysis = class_analysis->methods[j];
-                    printf("%s\n", method_analysis->full_name);
-                    auto basic_blocks = method_analysis->basic_blocks;
-                    for (uint32_t z = 0; z < basic_blocks->n_of_blocks; z++) {
-                        auto basic_block = basic_blocks->blocks[z];
-                        printf("%s\n", basic_block.block_string);
-                        [[maybe_unused]] auto data = methods[method_analysis->full_name][z].data();
-                        assert(strcmp(data, basic_block.block_string) == 0 && "Error, basic block disassembly is not correct");
+                let class_name = class.class_name();
+                assert_eq!(class_name, "DexParserTest");
+
+                let class_analysis = context.get_analyzed_class(class_name);
+                assert!(class_analysis.is_some());
+                let class_analysis = class_analysis.unwrap();
+
+                for method_analysis in class_analysis.methods() {
+                    let basic_blocks = method_analysis.basic_blocks();
+
+                    for (block_idx, block) in basic_blocks.blocks().iter().enumerate() {
+                        let data = methods.get(method_analysis.full_name());
+                        assert!(data.is_some());
+
+                        let data = data.unwrap();
+                        assert_eq!(block.block_string()
+                                        .split("\n")
+                                        .zip(data)
+                                        .filter(|&(a, b)| a != *b)
+                                        .map(|x| println!("{x:?}"))
+                                        .count(),
+                            0
+                        );
                     }
                 }
-                */
             }
-
-            assert!(false);
         }
     }
 }
