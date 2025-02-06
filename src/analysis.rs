25979921ccf1a61350a019b8561f5a8c6fad95c8
Julien Gamba <julien@jgamba.eu>
2025-02-06 13:44:50 +0100

    Getters in analysis.rs

--- src/analysis.rs
+++ src/analysis.rs
@@ -73,6 +73,21 @@ impl DvmClassMethodIdx {
             idx: ptr.idx as u64
         }
     }
+
+    /// Return a reference to the class of the struct
+    pub fn class(&self) -> &str {
+        &self.class
+    }
+
+    /// Return a reference to the method of the struct
+    pub fn method(&self) -> &str {
+        &self.method
+    }
+
+    /// Return the index
+    pub fn idx(&self) -> u64 {
+        self.idx
+    }
 }
 
 /// Type alias for Shuriken's `hdvm_method_idx_t`
@@ -99,6 +114,16 @@ impl DvmMethodIdx {
             method,
             idx: ptr.idx as u64
         }
+    }
+
+    /// Return a reference to the method of the XRef
+    pub fn method(&self) -> &str {
+        &self.method
+    }
+
+    /// Return the idx
+    pub fn idx(&self) -> u64 {
+        self.idx
     }
 }
 
@@ -137,6 +162,21 @@ impl DvmClassFieldIdx {
             idx: ptr.idx as u64
         }
     }
+
+    /// Return a reference to the class of the XRef
+    pub fn class(&self) -> &str {
+        &self.class
+    }
+
+    /// Return a reference to the field of the XRef
+    pub fn field(&self) -> &str {
+        &self.field
+    }
+
+    /// Return the idx
+    pub fn idx(&self) -> u64 {
+        self.idx
+    }
 }
 
 /// Type alias for Shuriken's `hdvm_class_idx_t`
@@ -163,6 +203,16 @@ impl DvmClassIdx {
             class,
             idx: ptr.idx as u64
         }
+    }
+
+    /// Return a reference to the class of the XRef
+    pub fn class(&self) -> &str {
+        &self.class
+    }
+
+    /// Return the idx
+    pub fn idx(&self) -> u64 {
+        self.idx
     }
 }
 
@@ -211,6 +261,21 @@ impl DvmRefTypeMethodIdx {
             idx: ptr.idx
         }
     }
+
+    /// Return the reference type
+    pub fn ref_type(&self) -> DvmRefType {
+        self.ref_type
+    }
+
+    /// Return a reference to the method name
+    pub fn method(&self) -> &str {
+        &self.method
+    }
+
+    /// Return the index
+    pub fn idx(&self) -> u64 {
+        self.idx
+    }
 }
 
 /// Type alias for Shuriken's `hdvm_classxref_t`
@@ -247,6 +312,21 @@ impl DvmClassXref {
             n_of_reftype_method_idx: ptr.n_of_reftype_method_idx,
             methods_xrefs,
         }
+    }
+
+    /// Return a reference to the class name
+    pub fn class(&self) -> &str {
+        &self.class
+    }
+
+    /// Return the number of methods references
+    pub fn n_of_reftype_method_idx(&self) -> usize {
+        self.n_of_reftype_method_idx
+    }
+
+    /// Return a reference to the methods
+    pub fn methods_xrefs(&self) -> &[DvmRefTypeMethodIdx] {
+        &self.methods_xrefs
     }
 }
 
@@ -320,6 +400,42 @@ impl DvmBasicBlock {
             block_string
         }
     }
+
+    /// Return the number of instructions in the block
+    pub fn n_of_instructions(&self) -> usize {
+        self.n_of_instructions
+    }
+
+    /// Return a reference to the pointer to the instructions in the block
+    pub fn instructions(&self) -> &[DvmInstruction] {
+        &self.instructions
+    }
+
+    /// Return the is it a try block?
+    pub fn try_block(&self) -> bool {
+        self.try_block
+    }
+
+    /// Return the is it a catch block
+    pub fn catch_block(&self) -> bool {
+        self.catch_block
+    }
+
+    /// Return a reference to the string value of the handler type
+    pub fn handler_type(&self) -> &str {
+        &self.handler_type
+    }
+
+    /// Return a reference to the name of the basic block
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Return a reference to the whole representation of a basic block in string format
+    pub fn block_string(&self) -> &str {
+        &self.block_string
+    }
+
 }
 
 /// Type alias for Shuriken's `basic_blocks_t`
@@ -327,7 +443,9 @@ impl DvmBasicBlock {
 /// Structure to keep all the basic blocks
 #[derive(Debug)]
 pub struct DvmBasicBlocks {
+    /// Number of basic blocks
     n_of_blocks: usize,
+    /// Vector of basic blocks
     blocks: Vec<DvmBasicBlock>
 }
 
@@ -344,6 +462,16 @@ impl DvmBasicBlocks {
             n_of_blocks: ptr.n_of_blocks,
             blocks
         }
+    }
+
+    /// Return the number of basic blocks
+    pub fn n_of_blocks(&self) -> usize {
+        self.n_of_blocks
+    }
+
+    /// Return a reference to the vector of basic blocks
+    pub fn blocks(&self) -> &[DvmBasicBlock] {
+        &self.blocks
     }
 }
 
@@ -394,6 +522,31 @@ impl DvmFieldAnalysis {
             n_of_xrefwrite: ptr.n_of_xrefwrite,
             xrefwrite
         }
+    }
+
+    /// Return a reference to the full name of the FieldAnalysis
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Return the number of xrefread
+    pub fn n_of_xrefread(&self) -> usize {
+        self.n_of_xrefread
+    }
+
+    /// Return a reference to the xrefread
+    pub fn xrefread(&self) -> &[DvmClassMethodIdx] {
+        &self.xrefread
+    }
+
+    /// Return the number of xrefwrite
+    pub fn n_of_xrefwrite(&self) -> usize {
+        self.n_of_xrefwrite
+    }
+
+    /// Return a reference to the xrefwrite
+    pub fn xrefwrite(&self) -> &[DvmClassMethodIdx] {
+        &self.xrefwrite
     }
 }
 
@@ -561,6 +714,111 @@ impl DvmMethodAnalysis {
             method_string
         }
     }
+
+    /// Return a reference to the name of the method
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Return a reference to the descriptor of the method
+    pub fn descriptor(&self) -> &str {
+        &self.descriptor
+    }
+
+    /// Return a reference to the full name of the method including class name and descriptor
+    pub fn full_name(&self) -> &str {
+        &self.full_name
+    }
+
+    /// Return the flag indicating if the method is external or not
+    pub fn external(&self) -> bool {
+        self.external
+    }
+
+    /// Return the flag indicating if the method is an android API
+    pub fn is_android_api(&self) -> bool {
+        self.is_android_api
+    }
+
+    /// Return a reference to the access flags
+    pub fn access_flags(&self) -> &[DvmAccessFlag] {
+        &self.access_flags
+    }
+
+    /// Return a reference to the class name
+    pub fn class_name(&self) -> &str {
+        &self.class_name
+    }
+
+    /// Return a reference to the basic blocks
+    pub fn basic_blocks(&self) -> &DvmBasicBlocks {
+        &self.basic_blocks
+    }
+
+    /// Return the number of field read in method
+    pub fn n_of_xrefread(&self) -> usize {
+        self.n_of_xrefread
+    }
+
+    /// Return a reference to the xrefs of field read
+    pub fn xrefread(&self) -> &[DvmClassFieldIdx] {
+        &self.xrefread
+    }
+
+    /// Return the number of field write
+    pub fn n_of_xrefwrite(&self) -> usize {
+        self.n_of_xrefwrite
+    }
+
+    /// Return a reference to the xrefs of field write
+    pub fn xrefwrite(&self) -> &[DvmClassFieldIdx] {
+        &self.xrefwrite
+    }
+
+    /// Return the number of xrefto
+    pub fn n_of_xrefto(&self) -> usize {
+        self.n_of_xrefto
+    }
+
+    /// Return a reference to the methods called from the current method
+    pub fn xrefto(&self) -> &[DvmClassMethodIdx] {
+        &self.xrefto
+    }
+
+    /// Return the number of xreffrom
+    pub fn n_of_xreffrom(&self) -> usize {
+        self.n_of_xreffrom
+    }
+
+    /// Return a reference to the methods that call the current method
+    pub fn xreffrom(&self) -> &[DvmClassMethodIdx] {
+        &self.xreffrom
+    }
+
+    /// Return the number of xrefnewinstance
+    pub fn n_of_xrefnewinstance(&self) -> usize {
+        self.n_of_xrefnewinstance
+    }
+
+    /// Return a reference to the new instance of the method
+    pub fn xrefnewinstance(&self) -> &[DvmClassIdx] {
+        &self.xrefnewinstance
+    }
+
+    /// Return the number of xrefconstclass
+    pub fn n_of_xrefconstclass(&self) -> usize {
+        self.n_of_xrefconstclass
+    }
+
+    /// Return a reference to the use of const class
+    pub fn xrefconstclass(&self) -> &[DvmClassIdx] {
+        &self.xrefconstclass
+    }
+
+    /// Return a reference to the cache of method string
+    pub fn method_string(&self) -> &str {
+        &self.method_string
+    }
 }
 
 /// Type alias for Shuriken's `hdvmclassanalysis_t`
@@ -675,5 +933,80 @@ impl DvmClassAnalysis {
             n_of_xreffrom: ptr.n_of_xreffrom,
             xreffrom,
         }
+    }
+
+    /// Return the is external class?
+    pub fn is_external(&self) -> bool {
+        self.is_external
+    }
+
+    /// Return a reference to the name of the class it extends
+    pub fn extends(&self) -> &str {
+        &self.extends
+    }
+
+    /// Return a reference to the name of the class
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Return the number of methods
+    pub fn n_of_methods(&self) -> usize {
+        self.n_of_methods
+    }
+
+    /// Return a reference to the pointer to an array of methods
+    pub fn methods(&self) -> &[DvmMethodAnalysis] {
+        &self.methods
+    }
+
+    /// Return the number of fields
+    pub fn n_of_fields(&self) -> usize {
+        self.n_of_fields
+    }
+
+    /// Return a reference to the pointer to an array of fields
+    pub fn fields(&self) -> &[DvmFieldAnalysis] {
+        &self.fields
+    }
+
+    /// Return the number of xrefnewinstance
+    pub fn n_of_xrefnewinstance(&self) -> usize {
+        self.n_of_xrefnewinstance
+    }
+
+    /// Return a reference to the new instance of this class
+    pub fn xrefnewinstance(&self) -> &[DvmMethodIdx] {
+        &self.xrefnewinstance
+    }
+
+    /// Return the number of const class
+    pub fn n_of_xrefconstclass(&self) -> usize {
+        self.n_of_xrefconstclass
+    }
+
+    /// Return a reference to the use of const class of this class
+    pub fn xrefconstclass(&self) -> &[DvmMethodIdx] {
+        &self.xrefconstclass
+    }
+
+    /// Return the number of xrefto
+    pub fn n_of_xrefto(&self) -> usize {
+        self.n_of_xrefto
+    }
+
+    /// Return a reference to the classes that this class calls
+    pub fn xrefto(&self) -> &[DvmClassXref] {
+        &self.xrefto
+    }
+
+    /// Return the number of xreffrom
+    pub fn n_of_xreffrom(&self) -> usize {
+        self.n_of_xreffrom
+    }
+
+    /// Return a reference to the classes that call this class
+    pub fn xreffrom(&self) -> &[DvmClassXref] {
+        &self.xreffrom
     }
 }
